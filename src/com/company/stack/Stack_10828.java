ad614eac9ea850c8ed24af3f1ada21d8d02eefd4
LeeChungWoo <skcjddn@gmail.com>
2021-08-24 09:20:50 +0900

    문제 업데이트

---
+++ src/com/company/stack/Stack_10828.java
@@ -0,0 +1,28 @@
+package com.company.stack;
+
+public class Stack_10828 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void push() {
+
+    }
+
+    public static int pop() {
+        return 1;
+    }
+
+    public static int size() {
+        return 1;
+    }
+
+    public static int empty() {
+        return 1;
+    }
+
+    public static int top() {
+        return 1;
+    }
+}
